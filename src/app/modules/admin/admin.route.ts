17ff170712d7348ac71b2ab6af0b159fb4284a02
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-07-15 20:30:20 +0600

    setup init

--- src/app/modules/admin/admin.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { AdminController } from "./admin.controller";
-const router = express.Router();
-
-// Routes
-router.post("/create-admin", AdminController.createAdmin);
-router.post("/login", AdminController.login);
-
-export const AdminRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,37 +1,17 @@
-import express from "express";
-const router = express.Router();
-import { UserRoutes } from "../modules/user/user.route";
-import { CowRoutes } from "../modules/cow/cow.routes";
-import { OrderRoutes } from "../modules/order/order.routes";
-import { AuthRoutes } from "../modules/auth/auth.route";
-import { AdminRoutes } from "../modules/admin/admin.route";
+// import express from "express";
+// const router = express.Router();
+// // import { UserRoutes } from "../modules/user/user.route";
 
-// Routes
-const moduleRoutes = [
-  {
-    path: "/auth",
-    route: AuthRoutes,
-  },
-  {
-    path: "/users",
-    route: UserRoutes,
-  },
-  {
-    path: "/cows",
-    route: CowRoutes,
-  },
-  {
-    path: "/orders",
-    route: OrderRoutes,
-  },
-  {
-    path: "/admins",
-    route: AdminRoutes,
-  }
-];
+// // Routes
+// const moduleRoutes = [
+//   {
+//     path: "/users",
+//     route: UserRoutes,
+//   }
+// ];
 
-moduleRoutes.forEach((route) => {
-  router.use(route.path, route.route);
-});
+// moduleRoutes.forEach((route) => {
+//   router.use(route.path, route.route);
+// });
 
-export default router;
+// export default router;
